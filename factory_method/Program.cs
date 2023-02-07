bbc9c563bdda4379dc73099d6421768854997f24
Mike Hong <y6602016@gmail.com>
2023-02-06 19:42:29 -0500

    inner factory class

--- factory_method/Program.cs
+++ factory_method/Program.cs
@@ -4,17 +4,6 @@ namespace PatternDemoCore
 {
   public class Point
   {
-    // factory method
-    public static Point NewCartesianPoint(double x, double y)
-    {
-      return new Point(x, y);
-    }
-
-    public static Point NewPolarPoint(double rho, double theta)
-    {
-      return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-    }
-
     private double x, y;
 
     private Point(double x, double y)
@@ -27,15 +16,32 @@ namespace PatternDemoCore
     {
       return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
     }
+
+    public static Point Origin => new Point(0, 0); // property, return a new object whenever called
+    public static Point Origin2 = new Point(0, 0); // field, only one
+
+    // inner Factory class
+    public static class Factory
+    {
+      public static Point NewCartesianPoint(double x, double y)
+      {
+        return new Point(x, y);
+      }
+
+      public static Point NewPolarPoint(double rho, double theta)
+      {
+        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+      }
+    }
   }
 
   static class Program
   {
     static void Main(string[] args)
     {
-      var point = Point.NewCartesianPoint(1, 2);
+      var point = Point.Factory.NewCartesianPoint(1, 2);
       Console.WriteLine(point);
-      var point2 = Point.NewPolarPoint(1.0, Math.PI / 2);
+      var point2 = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
       Console.WriteLine(point2);
     }
   }
